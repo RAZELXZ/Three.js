dd5ce94cadc62faedcfac1c9d60d7598129680cb
Qifeng Li <879265509aaa@gmail.com>
2023-09-15 12:31:39 -0500

    picker

--- src/canvas/Backdrop.jsx
+++ src/canvas/Backdrop.jsx
@@ -11,21 +11,21 @@ const Backdrop = () => {
     ref={shadows}
     temporal
     frames={60}
-    alphaTest={0.85}
+    alphaTest={0.55}
     scae={10}
     rotation={[Math.PI / 2, 0, 0]}
     position={[0, 0, -0.14]}>
       <RandomizedLight
         amount={4}
         radius={9}
-        intensity={0.55}
-        ambient={0.25}
+        intensity={0.85}
+        ambient={0.55}
         position={[5,5,-10]}/>
       <RandomizedLight
         amount={4}
         radius={5}
-        intensity={0.25}
-        ambient={0.55}
+        intensity={0.55}
+        ambient={0.85}
         position={[-5,5,-9]}/>
     </AccumulativeShadows>
   )
--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -12,6 +12,27 @@ import {AIPicker, FilePicker, ColorPicker, Tab, CustomButton} from '../component
 
 const Customizer = () => {
   const snap = useSnapshot(state);
+  const [file, setFile] = useState('');
+  const [prompt, setPrompt] = useState('');
+  const [generationgImg, setGeneratingImg] = useState('');
+  const [activeEditorTab, setActiveEditorTab] = useState('');
+  const [activeFilterTab, setActiveFilterTab] = useState({
+    logoShirt: true,
+    stylishShirt: false,
+  });
+
+  const generateTabContent = () => {
+    switch (activeEditorTab) {
+      case "colorpicker":
+        return <ColorPicker/>
+      case "filepicker":
+        return <ColorPicker/>
+      case "aipicker":
+        return <ColorPicker/>
+      default:
+        return null;
+    }
+  }
 
   return (
     <AnimatePresence>
@@ -24,10 +45,11 @@ const Customizer = () => {
                   <Tab
                     key={tab.name}
                     tab={tab}
-                    handleClick={{}}
+                    handleClick={() => {}}
                   />
                 ))
                 }
+                {generateTabContent()}
               </div>
             </div>
           </motion.div>
@@ -46,7 +68,7 @@ const Customizer = () => {
                     tab={tab}
                     isFilterTab
                     isActiveTab = ""
-                    handleClick={{}}
+                    handleClick={() => {}}
                   />
           ))}
           </motion.div>
